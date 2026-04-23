637185ba5a1406d192bd9d6f39ed9fb6bb6c827c
AndreCastr0 <andrecsleite@gmail.com>
2026-04-23 18:01:56 +0000

    fix: corrige troca de implementações entre train_model e optimize_model

--- train_model.py
+++ train_model.py
@@ -2,4 +2,22 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-#insira seu código aqui
+
+def load_images():
+    #Carregar dataset MNIST (divisão de treino e validação pré definida)
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+    #Normalizando os pixels da imagem para intervalo 0-1
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+
+    # Adiciona dimensão do canal: (28, 28) para (28, 28, 1)
+    x_train = x_train[..., tf.newaxis]
+    x_test = x_test[..., tf.newaxis]
+
+    return (x_train, y_train), (x_test, y_test)
+
+def main():
+
+if __name__ == "__main__":
+    main()
