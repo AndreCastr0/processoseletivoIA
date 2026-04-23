88d09b8fbbadf910176316b68c8fb548e5db9711
AndreCastr0 <andrecsleite@gmail.com>
2026-04-23 02:53:41 +0000

    carregar dataset

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,23 @@
 import tensorflow as tf
 import os
 
-#insira seu código aqui
+
+def load_images():
+    #Carregar dataset MNIST (divisão de treino e validação pré definida)
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+    #Normalizando os pixels da imagem para intervalo 0-1
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+
+    # Adiciona dimensão do canal: (28, 28) para (28, 28, 1)
+    x_train = x_train[..., tf.newaxis]
+    x_test = x_test[..., tf.newaxis]
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+def main():
+
+if __name__ == "__main__":
+    main()
