8389fa8238192823471b570a27b131288f4e77fd
AndreCastr0 <andrecsleite@gmail.com>
2026-04-24 18:16:38 +0000

    feat: carrega modelo treinado

--- optimize_model.py
+++ optimize_model.py
@@ -1,3 +1,11 @@
 import tensorflow as tf
 import os
 
+def main():
+    print("Carregando modelo model.h5...")
+    model = tf.keras.models.load_model("model.h5")
+
+    
+
+if __name__ == "__main__":
+    main()
