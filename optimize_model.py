dc316ff0c7ce9ab0c88e1aa71a7df5728640899f
AndreCastr0 <andrecsleite@gmail.com>
2026-04-24 18:17:29 +0000

    feat: converte modelo para TFLite

--- optimize_model.py
+++ optimize_model.py
@@ -5,7 +5,16 @@ def main():
     print("Carregando modelo model.h5...")
     model = tf.keras.models.load_model("model.h5")
 
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+    #Dynamic Range Quantization: reduz a precisão dos valores para deixar o modelo mais leve 
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    tflite_model = converter.convert()
+
     
+    
+
 
 if __name__ == "__main__":
     main()
